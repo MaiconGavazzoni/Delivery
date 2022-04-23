f5f095a0ebedb34157a96006c07c8f8bc3adcbce
Maicon Gavazzoni <maicon@datasystemweb.com.br>
2022-04-23 12:30:15 -0300

    commit final

---
+++ src/middlewares/ensureAuthenticateDeliveryman.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticateDeliveryman(request: Request, response: Response, next: NextFunction) {
+  const authHeader = request.headers.authorization;
+
+
+  if (!authHeader) {
+    return response.status(401).json({ message: "Token expirado" });
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const { sub } = verify(token, process.env.SECRET_TOKEN_DELIVERYMAN) as IPayload;
+    request.id_deliveryman = parseInt(sub);
+
+    return next();
+  } catch (error) {
+    return response.status(401).json({ message: "Token inválido!" });
+  }
+
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
+import { ensureAuthenticateCLient } from "./middlewares/ensureAuthenticateCLient";
+import { ensureAuthenticateDeliveryman } from "./middlewares/ensureAuthenticateDeliveryman";
 import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController";
 import { AuthenticateClientController } from "./modules/account/authenticateUser/AuthenticateClientController";
 import { CreateClientController } from "./modules/clients/useCase/createClient/CreateClientController";
-import { CreateDeliveryController } from "./modules/deliveries/useCases/CreateDeliveryController";
-import { CreateDeliverymanController } from "./modules/deliveryman/UseCase/createDeliveryman/CreateDeliverymanController";
+import { FindAllDeliveriesController } from "./modules/clients/useCase/deliveries/FindAllDeliveriesController";
+import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController";
+import { FindAllAvailableController } from "./modules/deliveries/useCases/findAllAvailable/FindAllAvailableController";
+import { UpdateDeliverymanController } from "./modules/deliveries/useCases/updateDeliveryman/useCases/UpdateDeliverymanController";
+import { UpdateEndDateController } from "./modules/deliveries/useCases/updateEndDate/UpdateEndDateController";
+import { CreateDeliverymanController } from "./modules/deliveryman/useCase/createDeliveryman/CreateDeliverymanController";
+import { FindAllDeliveriesDeliverymanController } from "./modules/deliveryman/useCase/findAllDeliveries/FindAllDeliveriesDeliverymanController";
 
 const routes = Router();
 
@@ -13,13 +20,23 @@ const authenticateDeliverymanController = new AuthenticateDeliverymanController(
 const createClientController = new CreateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
 const createDeliveryController = new CreateDeliveryController();
+const findAllAvailableController = new FindAllAvailableController();
+const updateDeliverymanController = new UpdateDeliverymanController();
+const findAllDeliveriesController = new FindAllDeliveriesController();
+const findAllDeliveriesDeliverymanController = new FindAllDeliveriesDeliverymanController();
+const updateEndDateController = new UpdateEndDateController();
 
 routes.post("/client/authenticate/", authenticateClientController.handle);
 routes.post("/deliveryman/authenticate/", authenticateDeliverymanController.handle);
 
-routes.post("/client/", createClientController.handle)
-routes.post("/deliveryman/", createDeliverymanController.handle)
-routes.post("/delivery/", createDeliveryController.handle)
+routes.post("/client/", createClientController.handle);
+routes.post("/deliveryman/", createDeliverymanController.handle);
+routes.post("/delivery/", ensureAuthenticateCLient, createDeliveryController.handle);
+routes.get("/delivery/available", ensureAuthenticateDeliveryman, findAllAvailableController.handle);
+routes.put("/delivery/updateDeliveryman/:id", ensureAuthenticateDeliveryman, updateDeliverymanController.handle);
 
+routes.get("/client/deliveries", ensureAuthenticateCLient, findAllDeliveriesController.handle);
+routes.get("/deliveryman/deliveries", ensureAuthenticateDeliveryman, findAllDeliveriesDeliverymanController.handle);
+routes.put("/delivery/updateEndDate/:id", ensureAuthenticateDeliveryman, updateEndDateController.handle);
 
 export { routes };
