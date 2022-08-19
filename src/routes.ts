e3f84b80263449c5c6d0b1cffd24fb5371caae3a
Maicon Gavazzoni <maicon@datasystemweb.com.br>
2022-08-18 21:08:40 -0300

    teste referencia dependencia

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,16 @@
 import { Router } from "express";
-import { ensureAuthenticateCLient } from "./middlewares/ensureAuthenticateCLient";
-import { ensureAuthenticateDeliveryman } from "./middlewares/ensureAuthenticateDeliveryman";
-import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController";
-import { AuthenticateClientController } from "./modules/account/authenticateUser/AuthenticateClientController";
-import { CreateClientController } from "./modules/clients/useCase/createClient/CreateClientController";
-import { FindAllDeliveriesController } from "./modules/clients/useCase/deliveries/FindAllDeliveriesController";
-import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController";
-import { FindAllAvailableController } from "./modules/deliveries/useCases/findAllAvailable/FindAllAvailableController";
-import { UpdateDeliverymanController } from "./modules/deliveries/useCases/updateDeliveryman/useCases/UpdateDeliverymanController";
-import { UpdateEndDateController } from "./modules/deliveries/useCases/updateEndDate/UpdateEndDateController";
-import { CreateDeliverymanController } from "./modules/deliveryman/useCase/createDeliveryman/CreateDeliverymanController";
-import { FindAllDeliveriesDeliverymanController } from "./modules/deliveryman/useCase/findAllDeliveries/FindAllDeliveriesDeliverymanController";
+import { ensureAuthenticateCLient } from "@/middlewares/ensureAuthenticateCLient";
+import { ensureAuthenticateDeliveryman } from "@/middlewares/ensureAuthenticateDeliveryman";
+import { AuthenticateDeliverymanController } from "@/modules/account/authenticateDeliveryman/AuthenticateDeliverymanController";
+import { AuthenticateClientController } from "@/modules/account/authenticateUser/AuthenticateClientController";
+import { CreateClientController } from "@/modules/clients/useCase/createClient/CreateClientController";
+import { FindAllDeliveriesController } from "@/modules/clients/useCase/deliveries/FindAllDeliveriesController";
+import { CreateDeliveryController } from "@/modules/deliveries/useCases/createDelivery/CreateDeliveryController";
+import { FindAllAvailableController } from "@/modules/deliveries/useCases/findAllAvailable/FindAllAvailableController";
+import { UpdateDeliverymanController } from "@/modules/deliveries/useCases/updateDeliveryman/useCases/UpdateDeliverymanController";
+import { UpdateEndDateController } from "@/modules/deliveries/useCases/updateEndDate/UpdateEndDateController";
+import { CreateDeliverymanController } from "@/modules/deliveryman/useCase/createDeliveryman/CreateDeliverymanController";
+import { FindAllDeliveriesDeliverymanController } from "@/modules/deliveryman/useCase/findAllDeliveries/FindAllDeliveriesDeliverymanController";
 
 const routes = Router();
 
