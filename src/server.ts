57780ae512d30e1354470587a030ad885b7eb592
Maicon Gavazzoni <maicon@datasystemweb.com.br>
2022-08-18 21:15:02 -0300

    volta ./

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
-import { routes } from '@/routes';
+import { routes } from './routes';
 
 
 const app = express();
