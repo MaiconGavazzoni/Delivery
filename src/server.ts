0cd19c2a14430a57bed01f19e2fdfcf13146888e
Maicon Gavazzoni <maicon@datasystemweb.com.br>
2022-08-19 08:54:06 -0300

    vai8

--- src/server.ts
+++ src/server.ts
@@ -27,4 +27,6 @@ app.use((err: Error, request: Request, response: Response, next: NextFunction) =
 
 })
 
-app.listen(process.env.PORT || 3000, () => console.log('listening on port 3000 http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
